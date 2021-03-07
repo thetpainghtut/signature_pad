9102f226132520627c80362ea5e04fd4d95a6412
thetpainghtut <thetpainghtut.bf@gmail.com>
2021-03-07 16:49:39 +0630

    edit blade

--- resources/views/signature-pad.blade.php
+++ resources/views/signature-pad.blade.php
@@ -2,9 +2,10 @@
 <!DOCTYPE html>
 <html>
 
-<meta charset="utf-8">
 
 <head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title>Laravel Signature Pad Tutorial Example - ItSolutionStuff.com </title>
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.css">
   
